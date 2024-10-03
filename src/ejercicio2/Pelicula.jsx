2131e36682321b7eb3f643b40ff47dc6d8114e83
mariajosegrau <mariajosegrau.alu@iespacomolla.es>
2024-10-03 16:52:11 +0200

    inicio ejercicio2

---
+++ src/ejercicio2/Pelicula.jsx
@@ -0,0 +1,8 @@
+import "./Pelicula.css";
+
+const Pelicula = (props) => {
+    //javascript    
+    const {nombre, director, clasificacion, recaudacion, cartelera, nota, actores} =  (props) =>{
+        
+    }
+}
